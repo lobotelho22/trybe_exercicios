8f0bea2f3a747bc054c055f9a0b3d530f5a2a75c
lobotelho22 <eduardo.ocara@gmail.com>
2022-03-07 14:55:24 -0300

    inclusao do bloco 04, dia 01

---
+++ bloco_04_introducao_a_JavaScript/variaveis_fixacao_01.js
@@ -0,0 +1,15 @@
+let myName = "Eduardo";
+const birthCity = "Rio de Janeiro";
+let birthYear = 1981;
+
+console.log(myName);
+console.log(birthCity);
+console.log(birthYear);
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "Cabrobró";
+
+console.log(birthCity);
