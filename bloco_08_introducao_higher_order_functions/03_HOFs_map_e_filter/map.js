f1a1fd867eb39fd3539801b33d8b09ae6efd4e3f
lobotelho22 <eduardo.ocara@gmail.com>
2022-04-11 14:28:39 -0300

    execucao do exercicio 01

---
+++ bloco_08_introducao_higher_order_functions/03_HOFs_map_e_filter/map.js
@@ -0,0 +1,15 @@
+// O método map percorre um objeto array, aplicando modificações ou condições lógicas e as retorna em um novo array
+
+// Qual a diferença, afinal, de map para o forEach ? São duas principais:
+// -> O map aplica sobre os elementos de um array uma função e retorna um array novo, sem modificar o original;
+// -> A forEach não tem tal restrição. Ela pode modificar o array original e retorna nada por padrão - ela pode criar um array novo a partir de um antigo, pode simplesmente buscar por um elemento e retorná-lo, pode não retornar nada, enfim! Ela é genérica e pode fazer diversas coisas.
+
+const persons = [
+    { firstName: 'Maria', lastName: 'Ferreira' },
+    { firstName: 'João', lastName: 'Silva' },
+    { firstName: 'Antonio', lastName: 'Cabral' },
+  ];
+  
+  const fullNames = persons.map((person) => `${person.firstName} ${person.lastName}`);
+  
+  console.log(fullNames); // [ 'Maria Ferreira', 'João Silva', 'Antonio Cabral' ]
