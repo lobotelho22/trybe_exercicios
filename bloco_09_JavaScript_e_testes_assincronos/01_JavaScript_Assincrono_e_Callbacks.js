715acdb2a62accb40197a84c0aad6cfba13969c5
lobotelho22 <eduardo.ocara@gmail.com>
2022-04-19 15:48:34 -0300

    resolucao dos exercicios de fixacao

---
+++ bloco_09_JavaScript_e_testes_assincronos/01_JavaScript_Assincrono_e_Callbacks.js
@@ -0,0 +1,40 @@
+// O uso da função SetTimeOut (callback, intervalo para execução) serve para executar eventos em assincronia. Estes eventos, entre outras coisas, poderão ser importantes para a execução de testes, simulando carregamento de elementos em um página, por exemplo.
+
+const despesas = [
+  {
+    gym: 99,
+  },
+  {
+    ifood: 200,
+  },
+  {
+    phone: 60,
+  },
+  {
+    internet: 100,
+  },
+];
+
+const renda = 1000;
+
+const despesaMensal = (renda, despesas, callback) => {
+
+  const despesaTotal = callback(despesas);
+  const saldoFinal = renda - despesaTotal;
+
+  console.log(`Balanço do mês:
+    Recebido: R$${renda},00
+    Gasto: R$${despesaTotal},00
+    Saldo: R$${saldoFinal},00 `);
+};
+
+const somaDespesas = (despesas) => {
+  const custoItem = despesas.map((item) => Object.values(item));
+  const despesaTotal = custoItem.reduce((acc, curr) => acc += curr[0], 0);
+  return despesaTotal;
+};
+
+// acc é a sigla para accumulator (acumulador)
+// curr é a sigla para current (valor atual)
+
+despesaMensal(renda, despesas, somaDespesas);
