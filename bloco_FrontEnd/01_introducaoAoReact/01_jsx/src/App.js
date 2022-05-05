72c55f361c108cbc8e30dba5673cdd5a68db361c
lobotelho22 <eduardo.ocara@gmail.com>
2022-05-05 15:34:59 -0300

    adicionado exercicio 01

--- bloco_FrontEnd/01_introducaoAoReact/01_jsx/src/App.js
+++ bloco_FrontEnd/01_introducaoAoReact/01_jsx/src/App.js
@@ -1,8 +1,19 @@
 import './App.css';
-import Component from './Component';
+//import Component from './Component';
 
 function App() {
-  return < Component />;
+
+  const tasks = ['levantar cedo', 'escovar os dentes', 'Matemática!', 'JavaScript!'];
+  // const calma = tasks.map( (task) => <li>{task}</li>)
+  // console.log(calma);
+  const Task = (tasks) => {
+    const newList = tasks.map( (task) => <li>{task}</li>)
+    return <ol>{newList}</ol>
+  }
+
+  return ( <div>
+    {Task(tasks)}
+  </div> );
 }
 
 export default App;
