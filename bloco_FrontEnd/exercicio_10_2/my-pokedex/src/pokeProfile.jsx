e652d5d1d1484952e87bde2591dff701af3124e4
lobotelho22 <eduardo.ocara@gmail.com>
2022-05-07 20:31:06 -0300

    gerada lista dos pokemons

---
+++ bloco_FrontEnd/exercicio_10_2/my-pokedex/src/pokeProfile.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import pokemons from "./data"
+
+
+class PokeProfile extends React.Component {
+    render () {
+
+        
+        
+        const pokeUnique = pokemons.map((pokemon) => {
+            const { name, type, averageWeight, image: urlImage } = pokemon
+            const altText = `uma imagem do pokemon chamado ${name}`
+            return (
+                <article className="pokemon-card">
+                <h3>{name}</h3>
+                <p>{type}</p>
+                <img src={urlImage} alt={altText} />
+                <p>Peso médio: {averageWeight.value} {averageWeight.measurementUnit}</p>
+                </article>
+            )
+        })
+
+        return (
+            <section>
+                {pokeUnique}
+            </section>
+        )
+    }
+}
+
+export default PokeProfile
