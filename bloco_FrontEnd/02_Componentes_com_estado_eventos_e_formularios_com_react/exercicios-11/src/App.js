f8c8ce0c667b1aff2131b63472ccfe08aa8f47ae
lobotelho22 <eduardo.ocara@gmail.com>
2022-05-11 07:30:01 -0300

    modelo de implementação de estado com função que o altera

--- bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
+++ bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
@@ -2,22 +2,21 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor () {
+  constructor(props) {
     super()
+    this.state = {
+      numeroDeCliques: 0
+    }
     this.handleClick = this.handleClick.bind(this)
   }
 
-  handleClick () {
-    console.log(this);
-    console.log('clicou!')
+  handleClick() {
+    this.setState((prevState, props) => ({
+      numeroDeCliques: prevState.numeroDeCliques + 1
+    }))
   }
-  render () {
-    console.log(this)
-    return (
-    <div className="App">
-      <button onClick={this.handleClick}>Meu Botão</button>
-    </div>
-  );
+  render() {
+    return <button onClick={this.handleClick}>{this.state.numeroDeCliques}</button>
   }
 }
 
