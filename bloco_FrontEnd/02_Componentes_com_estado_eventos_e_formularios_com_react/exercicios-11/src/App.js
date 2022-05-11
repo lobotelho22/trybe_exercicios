188018dfe6f42fc92ed130bbd8e747e888870643
lobotelho22 <eduardo.ocara@gmail.com>
2022-05-10 23:00:37 -0300

    exercicio 11.1 (fixacao) feita referenciação ao this para a função hancleClick, no escopo da função constructor

--- bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
+++ bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
@@ -2,6 +2,11 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
+  constructor () {
+    super()
+    this.handleClick = this.handleClick.bind(this)
+  }
+
   handleClick () {
     console.log(this);
     console.log('clicou!')
