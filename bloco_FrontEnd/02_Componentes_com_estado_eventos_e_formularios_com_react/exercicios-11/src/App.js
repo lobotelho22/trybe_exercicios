abe6f9a2bb505df9a894b3b3aaa6f7861794add4
lobotelho22 <eduardo.ocara@gmail.com>
2022-05-10 22:41:06 -0300

    exercicio 11.1 (fixacao) funcao construida dentro da classe

--- bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
+++ bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
@@ -1,17 +1,22 @@
 import React from 'react';
 import './App.css';
 
-function theMessage(event) {
-  console.log('Toca Pintinho!');
-}
-
 class App extends React.Component {
+  constructor() {
+    super()
+    this.theMessage = this.theMessage.bind(this);
+  }
+  
+  theMessage(event) {
+    console.log('Toca Pintinho!');
+  }
+
   render () {
     return (
     <div className="App">
-      <button onClick={theMessage}>Toca Pintinho!</button>
-      <button onClick={theMessage}>Sambarilove!</button>
-      <button onClick={theMessage}>Sabo-lho!</button>
+      <button onClick={this.theMessage}>Toca Pintinho!</button>
+      <button onClick={this.theMessage}>Sambarilove!</button>
+      <button onClick={this.theMessage}>Sabo-lho!</button>
     </div>
   );
   }
