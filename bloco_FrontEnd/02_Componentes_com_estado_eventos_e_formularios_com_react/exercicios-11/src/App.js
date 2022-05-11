60cdc3ca68eb8408eaf630a7dfd2a015a586a269
lobotelho22 <eduardo.ocara@gmail.com>
2022-05-10 22:53:33 -0300

    exercicio 11.1 (fixacao) handleClick chamada sem constructor perde referencia no this

--- bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
+++ bloco_FrontEnd/02_Componentes_com_estado_eventos_e_formularios_com_react/exercicios-11/src/App.js
@@ -2,21 +2,15 @@ import React from 'react';
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super()
-    this.theMessage = this.theMessage.bind(this);
+  handleClick () {
+    console.log(this);
+    console.log('clicou!')
   }
-  
-  theMessage(event) {
-    console.log('Toca Pintinho!');
-  }
-
   render () {
+    console.log(this)
     return (
     <div className="App">
-      <button onClick={this.theMessage}>Toca Pintinho!</button>
-      <button onClick={this.theMessage}>Sambarilove!</button>
-      <button onClick={this.theMessage}>Sabo-lho!</button>
+      <button onClick={this.handleClick}>Meu Botão</button>
     </div>
   );
   }
