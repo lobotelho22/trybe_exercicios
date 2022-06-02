c23a6bec903fc932f02186b9152615bf0c276287
lobotelho22 <eduardo.ocara@gmail.com>
2022-06-02 19:34:03 -0300

    teste com getLabelText

--- bloco_14_RTL_primeiros_passos/testes-react/src/App.test.js
+++ bloco_14_RTL_primeiros_passos/testes-react/src/App.test.js
@@ -3,6 +3,6 @@ import App from './App';
 
 test('renders learn react link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  const emailFormEl = screen.getByLabelText(/email/i);
+  expect(emailFormEl).toBeInTheDocument();
 });
