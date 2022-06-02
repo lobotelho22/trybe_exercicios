b7da5cdf562c4f53f7a584bab59e31118b928832
lobotelho22 <eduardo.ocara@gmail.com>
2022-06-02 20:14:50 -0300

    teste com getByTestId, que exige a "marcação" do item testado com a tag dataTestId

--- bloco_14_RTL_primeiros_passos/testes-react/src/App.test.js
+++ bloco_14_RTL_primeiros_passos/testes-react/src/App.test.js
@@ -8,9 +8,17 @@ test('está renderizando na tela um campo de emmail?', () => {
   expect(emailFormEl).toHaveProperty('type', 'email');
 });
 
-it('está aparecendo um botão na tela?', () => {
+it('Verifica se aparecem dois botões', () => {
   render(<App />);
   const btnEl = screen.getAllByRole('button');
   // console.log(btnEl[1].value);
-  expect(btnEl[1].value).toBe("voltar");
+  expect(btnEl).toHaveLength(2);
+})
+
+it('verificando a existência do botão Enviar', () => {
+  render(<App />);
+  const btnSendEl = screen.getByTestId('id-send');
+  expect(btnSendEl).toBeInTheDocument();
+  expect(btnSendEl).toHaveProperty('type', 'button');
+  expect(btnSendEl).toHaveValue('Enviar');
 })
