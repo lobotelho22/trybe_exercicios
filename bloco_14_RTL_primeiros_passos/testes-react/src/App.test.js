04f2e630b9dcfbc1664da20253f37bf02eb02ac1
lobotelho22 <eduardo.ocara@gmail.com>
2022-06-03 07:42:30 -0300

    teste com userEvent

--- bloco_14_RTL_primeiros_passos/testes-react/src/App.test.js
+++ bloco_14_RTL_primeiros_passos/testes-react/src/App.test.js
@@ -1,4 +1,6 @@
+import React from 'react';
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import App from './App';
 
 test('está renderizando na tela um campo de emmail?', () => {
@@ -21,4 +23,22 @@ it('verificando a existência do botão Enviar', () => {
   expect(btnSendEl).toBeInTheDocument();
   expect(btnSendEl).toHaveProperty('type', 'button');
   expect(btnSendEl).toHaveValue('Enviar');
+})
+
+test('Verificando se o botão e o campo email estão funcionando', () => {
+  render(<App />);
+
+  const EMAIL_USER = 'email@email.com.br';
+
+  const textEmail = screen.getByTestId('id-email-user');
+  expect(textEmail).toBeInTheDocument();
+  expect(textEmail).toHaveTextContent(/Valor:/g);
+
+  const btnSend = screen.getByTestId('id-send');
+  const inputEmail = screen.getByLabelText('Email');
+  userEvent.type(inputEmail, EMAIL_USER);
+  userEvent.click(btnSend);
+
+  expect(inputEmail).toHaveValue('');
+  expect(textEmail).toHaveTextContent(`Valor: ${EMAIL_USER}`);
 })
